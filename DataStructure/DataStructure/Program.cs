9461fa2c409cd8d2e4886c411028a8fb131e6d6b
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-07 22:32:13 +0530

    Create A Stacks

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
---
+++ DataStructure/DataStructure/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Stack
+    {
+
+
+        public Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+            Console.WriteLine("{0} pushed to stack", data);
+        }
+        public void Display()
+        {
+
+            int i = 1;
+            int Size = 0;
+            Node temp = top;
+
+            Console.WriteLine();
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine($" {i} Element in Stack is : " + temp.data);
+                temp = temp.next;
+                Size = i;
+                i++;
+            }
+            Console.WriteLine("\nTotal Elements in Stack is : " + Size);
+        }
+    }
+}
